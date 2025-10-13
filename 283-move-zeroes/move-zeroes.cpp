1648a9cc02d32a99041906b95c801430db4b6296
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2025-10-13 13:42:17 -0400

    Time: 0 ms (100.00%) | Memory: 23.8 MB (55.36%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int pointer = 0;
+        for (int i=0; i<nums.size(); i++){
+           if (nums[i] != 0){
+                if (pointer != i){
+                    nums[pointer] = nums[i];
+                    nums[i] = 0;
+                }
+            pointer ++;
+           }
+           
+        }
+    }
+};
