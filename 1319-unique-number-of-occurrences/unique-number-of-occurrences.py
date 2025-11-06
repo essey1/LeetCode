434d120153d72c829f28e7193c5464188674bbb9
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2025-11-05 20:54:10 -0500

    Time: 4 ms (9.04%) | Memory: 17.9 MB (65.92%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,16 +1,18 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        freqMap = {}
-        for num in arr:
-            if num in freqMap:
-                freqMap[num] += 1
+        freq = {}
+        for i in arr:
+            if i in freq:
+                freq[i] += 1
             else:
-                freqMap[num] = 1
-        hashSet = set()
-        for num in list(freqMap.values()):
-            if num in hashSet:
+                freq[i] = 1
+
+        hashset = set()
+        for i in list(freq.values()):
+            if i in hashset:
                 return False
             else:
-                hashSet.add(num)
+                hashset.add(i)
+
         return True
-        print(list(freqMap.values()))
+
