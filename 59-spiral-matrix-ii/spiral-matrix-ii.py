d18d3ee1fc70ca56afe65cefeb8471ce791ce29b
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2025-11-11 21:23:22 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (25.46%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/spiral-matrix-ii.py
@@ -1,49 +1,45 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         """
-            L ->   R
-          T 1 2 3 4
-          T 0 0 0 0
-            0 0 0 0
-          B 0 0 0 0
-
-          When moving horizontally the left and right pointers will tell
-            us the range and top and bottom pointer will tell us which row to fill in
-
-        When moving vertically the top and bottom pointers will tell
-            us the range and right and left pointer will tell us which row to fill in
+          LR  
+        1 2 3
+    TB  8 9 4
+        7 6 5
         """
-
-        matrix = [[0] * n for _ in range(n)]
-        l = 0
-        right = n-1
-        t = 0
-        b = n-1
+        # Pre-allocating
+        matrix = []
+        for j in range(n):
+            matrix.append([0]*n)
+        
+        left, right = 0, n-1
+        top, bottom = 0, n-1
         counter = 1
 
-        while t <= b:
-            # fill in top row
-            for c in range(l, right+1):
-                matrix[t][c] = counter
-                counter += 1
-            t += 1
+        # loop until left doesn't cross right
+        while left <= right:
 
-            # fill in the right column
-            for r in range(t, b+1):
+            # moving from left to right
+            for c in range(left, right+1):
+                matrix[top][c] = counter
+                counter += 1
+            top += 1
+
+            # moving from top to bottom
+            for r in range(top, bottom+1):
                 matrix[r][right] = counter
-                counter +=1
+                counter += 1
             right -= 1
 
-            # fill in the bottom row (reverse)
-            for c in range(right, l-1, -1):
-                matrix[b][c] = counter
+            # moving from right to left
+            for c in range(right, left-1, -1):
+                matrix[bottom][c] = counter
                 counter += 1
-            b -= 1
+            bottom -= 1
 
-            # fill in the left column (reverse)
-            for r in range(b, t-1, -1):
-                matrix[r][l] = counter
+            # moving from bottom to top
+            for r in range(bottom, top-1, -1):
+                matrix[r][left] = counter
                 counter += 1
-            l += 1
+            left += 1
 
         return matrix
