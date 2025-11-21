21b36ffbd229dcd9e9167f1f2e8836e48a627595
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2025-11-20 23:33:12 -0500

    Time: 0 ms (100.00%) | Memory: 18.9 MB (5.01%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -5,19 +5,33 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # iterative method
-        cur1 = head
-        reverse = ListNode()
-        cur2 =  reverse
-        stack = []
-        while cur1:
-            stack.append(cur1.val)
-            cur1 = cur1.next
-        print(stack)
-        for i in range(len(stack)):
-            cur2.next = ListNode(stack.pop())
-            cur2 = cur2.next
+        # recursive method
 
-        return reverse.next
-
+        #base case
+        if head == None or head.next == None:
+            return head
         
+        #recusrsive case
+        rec_head = Solution.reverseList(self, head.next)
+
+        head.next.next = head
+
+        head.next = None
+
+        return rec_head
+
+
+        # # iterative method
+        # cur1 = head
+        # reverse = ListNode()
+        # cur2 =  reverse
+        # stack = []
+        # while cur1:
+        #     stack.append(cur1.val)
+        #     cur1 = cur1.next
+        # print(stack)
+        # for i in range(len(stack)):
+        #     cur2.next = ListNode(stack.pop())
+        #     cur2 = cur2.next
+
+        # return reverse.next
