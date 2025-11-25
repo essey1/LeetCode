379a19c5c94765bfbdd38b8c2e9ed5fb8dff096d
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2025-11-24 19:50:54 -0500

    Time: 13 ms (47.06%) | Memory: 17.8 MB (85.88%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,36 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # sort both and check for equality
-        # O(nlogn)
-        s = sorted(s)
-        t = sorted(t)
-        if s == t:
-            return True
-        else:
+        # # sort both and check for equality
+        # # O(nlogn)
+        # s = sorted(s)
+        # t = sorted(t)
+        # if s == t:
+        #     return True
+        # else:
+        #     return False
+
+        # use counter dictionary 
+        if len(s) != len(t):
             return False
+
+        freq = {}
+        # count characters in s
+        for c in s:
+            if c not in freq:
+                freq[c] = 1
+            else:
+                freq[c] += 1
+        
+        for c in t:
+            if c not in freq:
+                return False
+            else:
+                freq[c] -= 1
+                if freq[c] < 0:
+                    return False
+
+        # for k in freq:
+        #     if freq[k] != 0:
+        #         return False
+
+        return True
