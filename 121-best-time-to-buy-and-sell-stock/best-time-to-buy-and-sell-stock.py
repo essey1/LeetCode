9d702ea6110cff02b09b63f110ea92e30ccc79ef
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2026-02-11 11:37:32 -0500

    Time: 51 ms (62.11%) | Memory: 28.6 MB (44.60%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,29 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # brute force O(n^2) nested loop
-        # more optimal solution with two pointers
-        """
-        7 , 1 , 5 , 6 , 4
-                        lr
+        out = 0
 
-        while r<len prices:
-            if price of l < price of r:
-                output = max(price[r] - price[l], output)
-                r += 1
-            else:
-                l = r
-                r+=1
-        return ouput
-        """
-        l, r  = 0, 1
-        max_profit = 0
-
-        while r<len(prices):
+        l=0
+        for r in range(len(prices)):
             if prices[l] < prices[r]:
-                max_profit = max(max_profit, prices[r] - prices[l])
-                r += 1
+                out = max(out, prices[r] - prices[l])
             else:
-                l = r
-                r += 1
-
-        return max_profit
+                l=r
+        return out
