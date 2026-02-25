5b80747e346019f0d0ccb8d7cf638ba0f5a094bc
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2026-02-25 15:28:30 -0500

    Time: 6 ms (0.60%) | Memory: 19.5 MB (27.08%) - LeetSync

---
+++ 48-rotate-image/rotate-image.py
@@ -0,0 +1,30 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        rotate outer layer if the matric and then move in to the next
+        """
+
+        l = 0
+        r = len(matrix) - 1
+
+        while l < r:
+            for i in range(0, r-l):
+                t = l
+                b = r
+
+                #save topleft
+                tl = matrix[t][l+i]
+
+                matrix[t][l+i] = matrix[b-i][l]
+                print(matrix)
+                matrix[b-i][l] = matrix[b][r-i]
+                matrix[b][r-i] = matrix[t+i][r]
+                matrix[t+i][r] = tl
+
+            r -= 1
+            l += 1
+            t += 1
+            b -= 1
+        return matrix
+            
+        
