d185866431f44d5857c65283a7d038a9c0a35d0a
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2026-02-25 15:29:17 -0500

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.15%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -16,9 +16,11 @@ class Solution:
                 tl = matrix[t][l+i]
 
                 matrix[t][l+i] = matrix[b-i][l]
-                print(matrix)
+
                 matrix[b-i][l] = matrix[b][r-i]
+                
                 matrix[b][r-i] = matrix[t+i][r]
+                
                 matrix[t+i][r] = tl
 
             r -= 1
