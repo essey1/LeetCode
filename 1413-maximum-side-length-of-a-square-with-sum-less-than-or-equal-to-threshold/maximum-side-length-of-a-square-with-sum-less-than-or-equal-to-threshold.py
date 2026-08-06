f43cb165a8e551dd3655ada8f0f8424b8b6fdd5c
Essey Tesfamichael Araya <111381905+essey1@users.noreply.github.com>
2026-08-05 20:12:00 -0600

    Time: 387 ms (41.79%) | Memory: 26.4 MB (61.62%) - LeetSync

---
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+
+        # Prefix Sum
+        p_mat = [[0 for _ in range(len(mat[0])+1)] for _ in range(len(mat)+1)]
+        # prefix sum matrix
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
+                # prefix += left + top - overlap
+                p_mat[r+1][c+1] += mat[r][c] + p_mat[r][c+1] + p_mat[r+1][c] - p_mat[r][c]
+
+        # Binary Search
+        res = 0
+        l, r = 0, min(len(mat), len(mat[0]))
+        while l<=r:
+            mid = (l+r)//2
+            valid_square = False
+            for i in range(mid-1, len(mat)):
+                for j in range(mid-1, len(mat[0])):
+                    summ = p_mat[i+1][j+1] + p_mat[i-mid+1][j-mid+1] - p_mat[i-mid+1][j+1] - p_mat[i+1][j-mid+1]
+                    """
+                    0 0 0 0
+                    0 1 2 3
+                    0 1 2 2
+                    0 2 2 2
+                    """
+                    if summ <= threshold:
+                        valid_square = True
+                        break
+                if valid_square:
+                    break
+            if valid_square:
+                res = mid
+                l = mid+1
+            else:
+                r = mid-1
+        return res
+        
